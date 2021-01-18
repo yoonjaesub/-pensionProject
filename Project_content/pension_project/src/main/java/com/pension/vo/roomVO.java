0881c4e5d9f7dc3ba5174a3dfaf92c4a580ed8cf
yoonjaesub <65588370+yoonjaesub@users.noreply.github.com>
2021-01-18 22:52:30 +0900

    21/01/08
    19일 오프라인회의를 위한 백업
    객실보기/ 객실추가기능 추가

--- Project_content/pension_project/src/main/java/com/pension/vo/roomVO.java
+++ Project_content/pension_project/src/main/java/com/pension/vo/roomVO.java
@@ -1,5 +1,41 @@
 package com.pension.vo;
 
-public class roomVO {
+public class RoomVO {
 
+	private int r_number;
+	private String r_name;
+	private int r_price;
+	private String r_description;
+	private String r_guestsnum;
+	
+	public int getR_number() {
+		return r_number;
+	}
+	public void setR_number(int r_number) {
+		this.r_number = r_number;
+	}
+	public String getR_name() {
+		return r_name;
+	}
+	public void setR_name(String r_name) {
+		this.r_name = r_name;
+	}
+	public int getR_price() {
+		return r_price;
+	}
+	public void setR_price(int r_price) {
+		this.r_price = r_price;
+	}
+	public String getR_description() {
+		return r_description;
+	}
+	public void setR_description(String r_description) {
+		this.r_description = r_description;
+	}
+	public String getR_guestsnum() {
+		return r_guestsnum;
+	}
+	public void setR_guestsnum(String r_guestsnum) {
+		this.r_guestsnum = r_guestsnum;
+	}
 }
